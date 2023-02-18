6e2ecefbb9f15a0a3a723c7ce3da6618de07c362
tbilhaut <113522624+tbilhaut@users.noreply.github.com>
2023-02-18 12:42:09 +0100

    Create Exo2.php

---
+++ Exo2.php
@@ -0,0 +1,24 @@
+<pre>
+    <?php
+    echo "index2.php";
+class Personnage {
+    //propriété
+    private $Speudo;
+    private $Vie;
+    
+    //Methode
+    public function __construct($Speudo,$Vie){
+        $this->Vie = $Vie;
+        $this->Speudo = $Speudo;
+    }
+
+    public function  afficheVie() {
+        echo "Mon speudo est ".$this->Speudo;
+        echo "Ma vie est de ".$this->Vie;
+    }
+  
+
+}
+?>
+
+</pre>
